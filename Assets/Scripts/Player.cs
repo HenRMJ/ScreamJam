c9ecfd8e9955653ecd7a77cca30b9c9261cc6448
Paul Frank <phfrank@protonmail.com>
2023-04-22 23:47:13 -0400

    Add 3D Bell and Bloodvials

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 /// <summary>
@@ -13,8 +15,10 @@ public class Player : MonoBehaviour
 
     public bool CanSummon { get; private set; }
 
+    private int startingBlood;
     [SerializeField] private int blood;
     [SerializeField] private Hand playerHand;
+    [SerializeField] private Transform bloodLevel = null;
 
     private void Awake()
     {
@@ -32,6 +36,7 @@ public class Player : MonoBehaviour
         playerHand.OnCardSummoned += PlayerHand_OnCardSummoned;
         StartRoundState.OnStartRound += StartRoundState_OnStartRound;
         AttackState.OnAttackStateStarted += AttackState_OnAttackStateStarted;
+        startingBlood = blood;
     }
 
     private void AttackState_OnAttackStateStarted(object sender, EventArgs e)
@@ -71,12 +76,33 @@ public class Player : MonoBehaviour
     public void DealDamage(int damage)
     {
         blood -= damage;
+        StartCoroutine(UpdateBloodLevel());
     }
 
     public void Heal(int healAmount)
     {
         blood += healAmount;
+        StartCoroutine(UpdateBloodLevel());
     }
 
     public int GetBlood() => blood;
+
+    private IEnumerator UpdateBloodLevel()
+    {
+        float t = 0f;
+        float speed = 1f;
+
+        Vector3 startingBloodLevel = bloodLevel.localScale;
+        float newBloodPercent = (float)blood / (float)startingBlood;
+        Vector3 finalBloodLevel = new Vector3(1, newBloodPercent, 1);
+        while (t < 1)
+        {
+            t += Time.deltaTime / speed;
+            if (t > 1) { t = 1; }
+
+            bloodLevel.localScale = Vector3.Lerp(startingBloodLevel, finalBloodLevel, t);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
 }
